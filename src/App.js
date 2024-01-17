92dbb758fce62628f14b358e894e9d2755835ee6
tjdwns335 <http://github.com/tjdwns335>
2024-01-17 14:53:47 +0900

    1-7 부모자식 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-// 자식 컴포넌트
+import React from "react";
 function Child() {
-  return <div>나는 아들입니다.</div>
+  return <div>아들입니다.</div>
+}
+function Mother() {
+  return <Child />
+}
+function GrandFather() {
+  return <Mother />
 }
 function App() {
   return (
     <>
-      <Child />
-      <Child />
-      <Child />
-      <Child />
-      <Child />
+      <GrandFather />
     </>
-  );
-}
+  )
+};
+
 export default App;
