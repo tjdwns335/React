a721c0dd24973fc3767a9a12c26d46382ce40e69
tjdwns335 <http://github.com/tjdwns335>
2024-01-17 15:43:39 +0900

    1-8 JSX

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from "react";
-function Child() {
-  return <div>아들입니다.</div>
-}
-function Mother() {
-  return <Child />
-}
-function GrandFather() {
-  return <Mother />
-}
-function App() {
-  return (
-    <>
-      <GrandFather />
-    </>
-  )
-};
+import React from 'react'
 
-export default App;
+export default function App() {
+  const number = 11;
+  const pTagStyle = {
+    color: 'red',
+  }
+  return (
+    <div className="test-class">
+      <p>안녕하세요 리엑트입니다.</p>
+      {/* 주석을 사용하는 방법입니다 */}
+      {/* 삼항 연사자를 사용해볼게요! */}
+      <p style={pTagStyle}>
+        {
+          number > 10 ? number + "은/는 10보다 크다" : number + "은/는 10보다 작다"
+        }
+      </p>
+    </div>
+  )
+}
