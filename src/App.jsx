25db8cfb87d2ce6690b0dc63891e7fee4adbfd1a
tjdwns335 <http://github.com/tjdwns335>
2024-01-17 20:14:39 +0900

    1-13 state(2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [fruit, setFruit] = useState("");
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  // id가 필드가 변경될 경우
+  const onIdChangeHandler = (event) => {
+    setId(event.target.value);
+  }
+  // Password가 필드가 변경될 경우
+  const onPasswordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  }
 
   return (
     <div>
-      과일 : <input
-        value={fruit}
-        onChange={function (event) {
-          // console.log(event.target.value);
-          setFruit(event.target.value);
-        }}
+      아이디 : <input
+        type="text"
+        value={id}
+        onChange={onIdChangeHandler}
       />
-      <br /><br />
-      {fruit}
-    </div>
+      <br />
+      비밀번호 : <input
+        type="password"
+        value={password}
+        onChange={onPasswordChangeHandler}
+      />
+      <br />
+      <button onClick={function () {
+        password.map()
+        alert(`고개님이 입력하신 아이디는  ${id}  이며, 비밀번호는 ${password} 입니다.`);
+        setId('');
+        setPassword('');
+      }}>로그인</button>
+    </div >
   );
 }
 
