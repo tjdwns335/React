e2dbe2e3c0dd782ea61de502682b926d6da3c906
tjdwns335 <http://github.com/tjdwns335>
2024-01-17 16:31:30 +0900

    1-10 Props children

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from 'react'
-
-// props를 통해 부모 -> 자식 데이터가 전달됐다.
-function Son(props) {
-  return <div>나는 {props.gfName}의 손자입니다.</div>
-}
-// 부모 -> 자식 정보를 전달했다!
-function Mother(props) {
-  const gfName = props.grandFatherName;
-  return <Son gfName={gfName} />
-}
-
-function GrandFather() {
-  const name = '산타할아버지'
-  return <Mother grandFatherName={name} />
-}
+import Layout from 'Layout'
 
 function App() {
-  return <GrandFather />
+  return <Layout>
+    <div>App 컴포넌트 에서 보낸 값입니다.</div>
+  </Layout>
 }
+
 
 export default App
