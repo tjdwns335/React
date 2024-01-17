02167dbb8fe69a1a93c240a83c5ee5e0c0f987bf
tjdwns335 <http://github.com/tjdwns335>
2024-01-17 14:17:56 +0900

    1-6컴포넌트 소개

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
 function App() {
+
+  // <---- 자바스크립트 영역 ---->
+  const handleClick = () => {
+    alert('클릭');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    /* <---- HTML/JSX 영역  ---->*/
+    <div
+      style={{
+        height: '100vh',
+        display: ' flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      {/* 이곳에 퀴즈를 위한 html 코드를 작성해 주세요 */}
+      <p>이것이 내가 만든 App컴포넌트 입니다</p>
+      <button onClick={handleClick}>클릭!</button>
     </div>
   );
 }
