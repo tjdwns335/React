966ab980c8083c75e53d4de3f88a7963dfa9bfac
tjdwns335 <http://github.com/tjdwns335>
2024-01-17 14:27:47 +0900

    1-7 부모 자식 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
+// 자식 컴포넌트
+function Child() {
+  return <div>나는 아들입니다.</div>
+}
 function App() {
-
-  // <---- 자바스크립트 영역 ---->
-  const handleClick = () => {
-    alert('클릭');
-  }
   return (
-    /* <---- HTML/JSX 영역  ---->*/
-    <div
-      style={{
-        height: '100vh',
-        display: ' flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      {/* 이곳에 퀴즈를 위한 html 코드를 작성해 주세요 */}
-      <p>이것이 내가 만든 App컴포넌트 입니다</p>
-      <button onClick={handleClick}>클릭!</button>
-    </div>
+    <>
+      <Child />
+      <Child />
+      <Child />
+      <Child />
+      <Child />
+    </>
   );
 }
-
 export default App;
