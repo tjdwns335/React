96f9376d03f5c85215f4343ef5b0a436c23fe61d
tjdwns335 <http://github.com/tjdwns335>
2024-01-18 12:47:05 +0900

    1-14 불변성&순수함수

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,22 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
-
-  // id가 필드가 변경될 경우
-  const onIdChangeHandler = (event) => {
-    setId(event.target.value);
-  }
-  // Password가 필드가 변경될 경우
-  const onPasswordChangeHandler = (event) => {
-    setPassword(event.target.value);
-  }
-  function PasswordChange(arr) {
-    let temp = '';
-    for (let i = 0; i < arr.length; i++) {
-      if (typeof arr === 'string') {
-        temp += '*';
-      }
-    }
-    return temp;
-  }
-
+  const [obj, setObj] = useState({
+    name: 'joon',
+    age: '30',
+  });
   return (
     <div>
-      아이디 : <input
-        type="text"
-        value={id}
-        onChange={onIdChangeHandler}
-      />
-      <br />
-      비밀번호 : <input
-        type="password"
-        value={password}
-        onChange={onPasswordChangeHandler}
-      />
-      <br />
-      <button onClick={function () {
-        alert(`고개님이 입력하신 아이디는  ${id}  이며, 비밀번호는 ${PasswordChange(password)} 입니다.`);
-        setId('');
-        setPassword('');
-      }}>로그인</button>
+      <div>{obj.name}</div>
+      <button onClick={() => {
+        obj.name = 'joon2';
+        const obj2 = { ...obj };
+        setObj(obj2);
+        // 새로운 객체를 만들어야 한다.
+        // setObj(obj);
+      }}>변경!</button>
     </div >
-  );
+  )
 }
 
 export default App
